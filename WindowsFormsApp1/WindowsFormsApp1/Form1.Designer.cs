becff93be8f9539edcdf03bba4d4a63ca8ff8d57
Се Цзитао <115497425+mossXie@users.noreply.github.com>
2025-10-16 17:44:37 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb = new System.Windows.Forms.TextBox();
+            this.but0 = new System.Windows.Forms.Button();
+            this.butP = new System.Windows.Forms.Button();
+            this.butE = new System.Windows.Forms.Button();
+            this.butPM = new System.Windows.Forms.Button();
+            this.butDot = new System.Windows.Forms.Button();
+            this.butD = new System.Windows.Forms.Button();
+            this.butm = new System.Windows.Forms.Button();
+            this.butS = new System.Windows.Forms.Button();
+            this.butC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(70, 21);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(320, 20);
+            this.tb.TabIndex = 0;
+            this.tb.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // but0
+            // 
+            this.but0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but0.Location = new System.Drawing.Point(70, 146);
+            this.but0.Name = "but0";
+            this.but0.Size = new System.Drawing.Size(40, 40);
+            this.but0.TabIndex = 1;
+            this.but0.Text = "0";
+            this.but0.UseVisualStyleBackColor = true;
+            this.but0.Click += new System.EventHandler(this.but0_Click);
+            // 
+            // butP
+            // 
+            this.butP.Location = new System.Drawing.Point(294, 149);
+            this.butP.Name = "butP";
+            this.butP.Size = new System.Drawing.Size(38, 36);
+            this.butP.TabIndex = 2;
+            this.butP.Text = "+";
+            this.butP.UseVisualStyleBackColor = true;
+            this.butP.Click += new System.EventHandler(this.butp_Click_1);
+            // 
+            // butE
+            // 
+            this.butE.Location = new System.Drawing.Point(70, 354);
+            this.butE.Name = "butE";
+            this.butE.Size = new System.Drawing.Size(75, 23);
+            this.butE.TabIndex = 3;
+            this.butE.Text = "=";
+            this.butE.UseVisualStyleBackColor = true;
+            this.butE.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // butPM
+            // 
+            this.butPM.Location = new System.Drawing.Point(556, 218);
+            this.butPM.Name = "butPM";
+            this.butPM.Size = new System.Drawing.Size(75, 23);
+            this.butPM.TabIndex = 4;
+            this.butPM.Text = "+/-";
+            this.butPM.UseVisualStyleBackColor = true;
+            this.butPM.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // butDot
+            // 
+            this.butDot.Location = new System.Drawing.Point(556, 146);
+            this.butDot.Name = "butDot";
+            this.butDot.Size = new System.Drawing.Size(75, 23);
+            this.butDot.TabIndex = 5;
+            this.butDot.Text = ",";
+            this.butDot.UseVisualStyleBackColor = true;
+            this.butDot.Click += new System.EventHandler(this.butDot_Click);
+            // 
+            // butD
+            // 
+            this.butD.Location = new System.Drawing.Point(384, 149);
+            this.butD.Name = "butD";
+            this.butD.Size = new System.Drawing.Size(49, 36);
+            this.butD.TabIndex = 6;
+            this.butD.Text = "*";
+            this.butD.UseVisualStyleBackColor = true;
+            this.butD.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // butm
+            // 
+            this.butm.Location = new System.Drawing.Point(384, 205);
+            this.butm.Name = "butm";
+            this.butm.Size = new System.Drawing.Size(49, 36);
+            this.butm.TabIndex = 7;
+            this.butm.Text = "/";
+            this.butm.UseVisualStyleBackColor = true;
+            this.butm.Click += new System.EventHandler(this.butm_Click);
+            // 
+            // butS
+            // 
+            this.butS.Location = new System.Drawing.Point(294, 205);
+            this.butS.Name = "butS";
+            this.butS.Size = new System.Drawing.Size(38, 36);
+            this.butS.TabIndex = 8;
+            this.butS.Text = "-";
+            this.butS.UseVisualStyleBackColor = true;
+            this.butS.Click += new System.EventHandler(this.butS_Click);
+            // 
+            // butC
+            // 
+            this.butC.Location = new System.Drawing.Point(285, 354);
+            this.butC.Name = "butC";
+            this.butC.Size = new System.Drawing.Size(75, 23);
+            this.butC.TabIndex = 9;
+            this.butC.Text = "clear";
+            this.butC.UseVisualStyleBackColor = true;
+            this.butC.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butC);
+            this.Controls.Add(this.butS);
+            this.Controls.Add(this.butm);
+            this.Controls.Add(this.butD);
+            this.Controls.Add(this.butDot);
+            this.Controls.Add(this.butPM);
+            this.Controls.Add(this.butE);
+            this.Controls.Add(this.butP);
+            this.Controls.Add(this.but0);
+            this.Controls.Add(this.tb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button but0;
+        private System.Windows.Forms.Button butP;
+        private System.Windows.Forms.Button butE;
+        private System.Windows.Forms.Button butPM;
+        private System.Windows.Forms.Button butDot;
+        private System.Windows.Forms.Button butD;
+        private System.Windows.Forms.Button butm;
+        private System.Windows.Forms.Button butS;
+        private System.Windows.Forms.Button butC;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum CalcOp
+        {
+            None,
+            Add,
+            Sub,
+            Mul,
+            Div,
+        }
+        CalcOp prevOp = CalcOp.None;
+        bool NeedClear = true;
+        float prev = 0;
+        private void but0_Click(object sender, EventArgs e)
+        {
+            if (NeedClear)
+            {
+                tb.Text = "";
+                NeedClear = false;
+            }
+            if (tb.Text.Length < 10)
+                tb.Text += ((Button)sender).Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            palceBut(but0, 2, 3);
+            palceBut(but0, 1, 3);
+            for (int x=0; x<3; x++)
+                for (int y=0; y<3; y++)
+                {
+                    Button b = new Button();
+                    b.Parent = this;
+                    b.Text = (y * 3 + x + 1).ToString();
+                    b.Click += but0_Click;
+                    palceBut(b, x, y);
+                }
+
+        }
+        private void palceBut (Button b, int x, int y)
+        {
+            b.Size = new Size(40, 40);
+            b.Left = tb.Left + 45 * x;
+            b.Top = tb.Top + +tb.Height + 5 + y * 45;
+            b.Font = new Font("Microsoft Sans Serif", 14f);
+        }
+        private void butP_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void butE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butp_Click_1(object sender, EventArgs e)
+        {
+            NeedClear = true;
+            prev = float.Parse(tb.Text);
+            prevOp = CalcOp.Add;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float v = float.Parse(tb.Text);
+            switch (prevOp)
+            {
+                case CalcOp.Add:
+                    tb.Text = (prev + v).ToString();
+                    break;
+                case CalcOp.Sub:
+                    tb.Text = (prev - v).ToString();
+                    break;
+                case CalcOp.Div:
+                    tb.Text = (prev / v).ToString();
+                    break;
+                case CalcOp.Mul:
+                    tb.Text = (prev * v).ToString();
+                    break;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (tb.Text[0] == '-')
+            {
+                tb.Text = tb.Text.Substring(1);
+
+            }
+            else {
+
+                tb.Text = "-" + tb.Text;
+            }
+        }
+
+        private void butDot_Click(object sender, EventArgs e)
+        {
+            if (NeedClear)
+            {
+                tb.Text = "0";
+                NeedClear = false;
+            }
+            if(tb.Text.Length < 9) {
+                tb.Text += ",";
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            NeedClear = true;
+            prev = float.Parse(tb.Text);
+            prevOp = CalcOp.Mul;
+        }
+
+        private void butS_Click(object sender, EventArgs e)
+        {
+            NeedClear = true;
+            prev = float.Parse(tb.Text);
+            prevOp = CalcOp.Sub;
+        }
+
+        private void butm_Click(object sender, EventArgs e)
+        {
+            NeedClear = true;
+            prev = float.Parse(tb.Text);
+            prevOp = CalcOp.Div;
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            tb.Text = " ";
+            NeedClear = false;
+        }
+    }
+}
